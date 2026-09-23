82bf1673214357ee5af7d78af655e8f22e328a0b
meshack-con <178539501+meshack-con@users.noreply.github.com>
2026-09-23 10:22:03 +0300

    Refactor flutter build command in build.sh
    Refactor flutter build command for improved readability.

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:$(pwd)/flutter/bin"
 flutter config --enable-web
 flutter pub get
-flutter build web --release --dart-define=API_BASE_URL=https://nyumba-mkononi-backend-itzt.onrender.com
+flutter build web --release \
+  --dart-define=API_BASE_URL=https://nyumba-mkononi-backend-itzt.onrender.com
